aa83de77eb801e0890e958c1400831e2b3d22bb3
Dav Sargsyan <davidsargsyandev@gmail.com>
2020-07-27 22:00:17 +0400

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/davo0779/04dcf445-c00b-4f90-9596-9f70c667304e/Project JobLess/API/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
